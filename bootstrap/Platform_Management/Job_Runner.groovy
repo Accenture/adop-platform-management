b6e9974e602f9f59b5093db7c2dc4086a1852d7e
dharmanshu.singh <dharmanshu.singh@accenture.com>
2017-01-17 17:40:32 +0000

    Adding parent job to call platform setup jobs

---
+++ bootstrap/Platform_Management/Job_Runner.groovy
@@ -0,0 +1,29 @@
+// Constants
+
+def platformManagementFolderName= "/Platform_Management"
+def platformManagementFolder = folder(platformManagementFolderName) { displayName('Platform Management') }
+
+// Jobs
+def jobRunner = workflowJob(platformManagementFolderName + "/Job_Runner")
+ 
+// Setup setup_cartridge
+jobRunner.with{
+    description("This job is responsible for executing all required jobs under the Platform_Management folder to setup your platform")
+    properties {
+        rebuild {
+            autoRebuild(false)
+            rebuildDisabled(false)
+        }
+    }
+    definition {
+        cps {
+            script('''// Load the list of default cartridges
+build job: 'Platform_Management/Load_Cartridge_List'
+
+// Setup the Gerrit ACL
+build job: 'Platform_Management/Setup_Gerrit'
+''')
+sandbox()
+        }
+    }
+} 
